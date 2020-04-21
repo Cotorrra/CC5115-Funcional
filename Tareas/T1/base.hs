ce9529ee08c0911840af4c328674318db1d400f5
Amitsuu <joaquinpereza96@gmail.com>
2020-04-21 01:37:40 -0400

    T1 base

---
+++ Tareas/T1/base.hs
@@ -0,0 +1,109 @@
+import Test.Hspec
+import Test.QuickCheck
+
+
+
+{-------------------------------------------}
+{--------------  EJERCICIO 1  --------------}
+{-------------------------------------------}
+
+-- effPow ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+-- pay ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+
+
+
+{-------------------------------------------}
+{--------------  EJERCICIO 2  --------------}
+{-------------------------------------------}
+
+-- numberOfHits ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+-- splitAtFirstHit ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+-- positionsAllHits ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+-- evens ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+-- odds ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+
+
+
+{-------------------------------------------}
+{--------------  EJERCICIO 3  --------------}
+{-------------------------------------------}
+
+-- hasSomeHit ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+-- isMember ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+-- repeteadElem ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+-- applyUntil ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+-- leastPow2 ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+-- balancedSufix ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+
+
+
+{-------------------------------------------}
+{--------------  EJERCICIO 4  --------------}
+{-------------------------------------------}
+
+{-
+Provea derivacion de isMemberPF
+-}
+
+-- isMemberPF ::
+-- complete tipo y definicion, y descomente la linea de arriba
+
+
+
+
+{-------------------------------------------}
+{--------------  EJERCICIO 5  --------------}
+{-------------------------------------------}
+
+-- testEffPow :: Spec
+-- complete definicion y descomente la linea de arriba
+
+-- testPay :: Spec
+-- complete definicion y descomente la linea de arriba
+
+-- testNumberOfHits :: Spec
+-- complete definicion y descomente la linea de arriba
+
+-- testExtraNumberOfHits :: Spec
+-- complete definicion y descomente la linea de arriba
+
+
+main :: IO()
+main = pure ()
+{-
+Luego de resolver el Ejercicio 5, elimine
+la definicion de main de arriba, y cambiela
+por la siguiente:
+
+main = hspec $ do
+  testEffPow
+  testPay
+  testNumberOfHits
+  testExtraNumberOfHits
+-}
