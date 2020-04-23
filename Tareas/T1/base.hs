7959e0c9a21262085cabda061602e5d761e19577
Amitsuu <joaquinpereza96@gmail.com>
2020-04-23 03:00:02 -0400

    Progreso T1

--- Tareas/T1/base.hs
+++ Tareas/T1/base.hs
@@ -2,38 +2,59 @@ import Test.Hspec
 import Test.QuickCheck
 
 
-
 {-------------------------------------------}
 {--------------  EJERCICIO 1  --------------}
 {-------------------------------------------}
 
--- effPow ::
--- complete tipo y definicion, y descomente la linea de arriba
+effPow :: Num a => a -> Int -> a
+effPow b 1 = b
+effPow b n | n < 0 = error "exponente negativo" 
+           | even n = (effPow b m) * (effPow b m) 
+           | otherwise = (effPow b m) * (effPow b m) * b where
+            m = case even n of
+              True -> n `quot` 2
+              False -> (n - 1) `quot` 2
 
--- pay ::
--- complete tipo y definicion, y descomente la linea de arriba
+pay :: Int -> (Int, Int)
+pay 8 = (1,1)
+pay 9 = (3,0)
+pay 10 = (0,2)
+pay n | n < 8 = error "no se pueden pagar " n " pesos"
+      | otherwise = addTuple (1,0) (pay (n - 3))
 
 
+addTuple :: (Int, Int) -> (Int, Int) -> (Int, Int)
+addTuple (a,b) (c,d) = (a+c, b+d)
 
 
 {-------------------------------------------}
 {--------------  EJERCICIO 2  --------------}
 {-------------------------------------------}
 
--- numberOfHits ::
--- complete tipo y definicion, y descomente la linea de arriba
+numberOfHits :: (a -> Bool) -> [a] -> Int
+numberOfHits _ [] = 0
+numberOfHits p (x:xs) | p x = 1 + numberOfHits p xs
+                      | otherwise = 0 + numberOfHits p xs
+{-
+splitAtFirstHit :: (a -> Bool) -> [a] -> ([a],[a])
+-- splitAtFirstHit _ [] = ...
+splitAtFirstHit _ x = 12 
+splitAtFirstHit p (x:xs) | p x = [x,xs] -- magic split
+                         | otherwise = [x] ++ splitAtFirstHit p xs
+-}
 
--- splitAtFirstHit ::
--- complete tipo y definicion, y descomente la linea de arriba
 
 -- positionsAllHits ::
 -- complete tipo y definicion, y descomente la linea de arriba
 
--- evens ::
--- complete tipo y definicion, y descomente la linea de arriba
 
--- odds ::
--- complete tipo y definicion, y descomente la linea de arriba
+evens :: [a] -> [a]
+evens [] = []
+evens (x:xs) = [x] ++ (odds xs)
+
+odds :: [a] -> [a]
+odds [] = []
+odds (_:xs) = evens xs
 
 
 
@@ -42,22 +63,32 @@ import Test.QuickCheck
 {--------------  EJERCICIO 3  --------------}
 {-------------------------------------------}
 
--- hasSomeHit ::
--- complete tipo y definicion, y descomente la linea de arriba
+hasSomeHit :: (a -> Bool) -> [a] -> Bool
+hasSomeHit _ [] = False
+hasSomeHit p (x:xs) | p x = True
+                    | otherwise = hasSomeHit p xs
 
--- isMember ::
--- complete tipo y definicion, y descomente la linea de arriba
+isMember :: Eq a => a -> [a] -> Bool
+isMember x xs = hasSomeHit (\y -> x == y) xs
 
--- repeteadElem ::
--- complete tipo y definicion, y descomente la linea de arriba
 
--- applyUntil ::
--- complete tipo y definicion, y descomente la linea de arriba
+repeteadElem :: Eq a => [a] -> Bool
+repeteadElem [] = False
+repeteadElem (x:xs) = (isMember x xs) && repeteadElem xs
 
--- leastPow2 ::
--- complete tipo y definicion, y descomente la linea de arriba
 
--- balancedSufix ::
+applyUntil :: (a -> a) -> (a -> Bool) -> a -> a
+applyUntil f p b | p b = b
+                 | otherwise = applyUntil f p (f b)
+
+leastPow2 :: Int -> Int
+leastPow2 n | n < 0 = error "argumento negativo"
+            | otherwise = applyUntil (\x -> effPow x 2) (\y -> y > n) 2
+
+
+-- balancedSufix :: [Bool] -> [Bool]
+-- balancedSufix xs | 
+
 -- complete tipo y definicion, y descomente la linea de arriba
 
 
@@ -81,29 +112,39 @@ Provea derivacion de isMemberPF
 {--------------  EJERCICIO 5  --------------}
 {-------------------------------------------}
 
--- testEffPow :: Spec
--- complete definicion y descomente la linea de arriba
+testEffPow :: Spec
+testEffPow = describe "testEffPow function:" $ do
+    it "La potencia de la multiplicacion es la multiplicacion de la potencia" $
+        property $ \x y n -> (n::Int) > 0 ==> effPow ((x::Int) * (y::Int)) n == (effPow x n) * (effPow y n)
+    it "La suma de exponentes es la multiplicacion de potencias" $
+        property $ \x m n -> ((n::Int) > 0) ==> ((m::Int) > 0) ==> effPow (x::Int) (n + m) == (effPow x m) * (effPow x n)
+    
 
--- testPay :: Spec
--- complete definicion y descomente la linea de arriba
+testPay :: Spec
+testPay = describe "testPay function:" $ do
+    it "" $
+        property $ \n -> (n::Int) >= 8 ==> (3 * (fst (pay n))) + (5 * (snd (pay n))) == n
+    
+testNumberOfHits :: Spec
+testNumberOfHits = describe "testNumberOfHits function:" $ do
+    it "" $
+      property $ \xs ys -> numberOfHits even ((xs::[Int]) ++ (ys::[Int])) == numberOfHits even xs + numberOfHits even ys
+    it "" $
+      property $ \xs -> numberOfHits (\_ -> True) (xs::[Int]) == length xs
+    it "" $
+      property $ \xs -> numberOfHits (\_ -> False) (xs::[Int])  == 0
 
--- testNumberOfHits :: Spec
--- complete definicion y descomente la linea de arriba
-
--- testExtraNumberOfHits :: Spec
--- complete definicion y descomente la linea de arriba
-
-
-main :: IO()
-main = pure ()
 {-
-Luego de resolver el Ejercicio 5, elimine
-la definicion de main de arriba, y cambiela
-por la siguiente:
+testExtraNumberOfHits :: Spec
+testExtraNumberOfHits = describe "testNumberOfHits function:" $ do
+    it "" $
+      property $ 
+-}
 
+main:: IO()
 main = hspec $ do
   testEffPow
   testPay
   testNumberOfHits
-  testExtraNumberOfHits
--}
+  -- testExtraNumberOfHits
+
