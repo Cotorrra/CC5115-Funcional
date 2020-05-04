2adaa1a88ee1c4aca224e96d84bcc7c28825c64f
Amitsuu <joaquinpereza96@gmail.com>
2020-05-04 05:26:28 -0400

    Finalizada Tarea 1

--- Tareas/T1/base.hs
+++ Tareas/T1/base.hs
@@ -1,6 +1,6 @@
 import Test.Hspec
 import Test.QuickCheck
-
+import Test.Hspec.Core.QuickCheck(modifyMaxSuccess)
 
 {-------------------------------------------}
 {--------------  EJERCICIO 1  --------------}
@@ -24,7 +24,7 @@ pay n | n < 8 = error "no se pueden pagar " n " pesos"
 
 
 addTuple :: (Int, Int) -> (Int, Int) -> (Int, Int)
-addTuple (a,b) (c,d) = (a+c, b+d)
+addTuple (a,b) (c,d) = (a + c, b + d)
 
 
 {-------------------------------------------}
@@ -43,13 +43,13 @@ splitAtFirstHit p (x:xs)
           | length result == 2 = (head result, head (tail result))
           | length result == 1 = error "no hit in the list"
           | otherwise = error "cursed error"
-          where result = splitAtFirstHit2 p (x:xs)
+          where result = splitAtFirstHit2 p (x:xs) []
 
 
-splitAtFirstHit2 :: (a -> Bool) -> [a] -> [[a]]
-splitAtFirstHit2 _ [] = []
-splitAtFirstHit2 p (x:xs) | p x = splitAtFirstHit2 (\_ -> False) xs
-                          | otherwise = [x] ++ (splitAtFirstHit2 (\_ -> False) xs)
+splitAtFirstHit2 :: (a -> Bool) -> [a] -> [a] -> [[a]]
+splitAtFirstHit2 _ [] ys = [ys]
+splitAtFirstHit2 p (x:xs) ys | p x = [ys] ++ (splitAtFirstHit2 (\_ -> False) xs [x])
+                             | otherwise = splitAtFirstHit2 p xs (ys ++ [x])
 
 
 positionsAllHits :: (a -> Bool) -> [a] -> [Int]
@@ -79,30 +79,32 @@ hasSomeHit _ [] = False
 hasSomeHit p (x:xs) | p x = True
                     | otherwise = hasSomeHit p xs
 
+
 isMember :: Eq a => a -> [a] -> Bool
-isMember x xs = hasSomeHit (\y -> x == y) xs
+isMember x xs = hasSomeHit (x==) xs
 
 
 repeteadElem :: Eq a => [a] -> Bool
 repeteadElem [] = False
-repeteadElem (x:xs) = (isMember x xs) && repeteadElem xs
+repeteadElem (x:xs) = (hasSomeHit (==x) xs) || repeteadElem xs
+-- repeteadElem (x:xs) = (isMember x xs) || repeteadElem xs
 
 
 applyUntil :: (a -> a) -> (a -> Bool) -> a -> a
 applyUntil f p b | p b = b
                  | otherwise = applyUntil f p (f b)
 
+
 leastPow2 :: Int -> Int
 leastPow2 n | n < 0 = error "argumento negativo"
             | otherwise = applyUntil (\x -> effPow x 2) (\y -> y > n) 2
 
 
--- balancedSufix :: [Bool] -> [Bool]
--- balancedSufix xs | 
-
--- complete tipo y definicion, y descomente la linea de arriba
-
-
+balancedSufix :: [Bool] -> [Bool]
+balancedSufix [] = []
+balancedSufix (x:xs) = applyUntil f p (x:xs)
+    where f = (\ys -> tail ys) -- Saco un sufijo de xs
+          p = (\ys -> ((numberOfHits (id) ys) == (numberOfHits (\y -> not y) ys)))
 
 
 {-------------------------------------------}
@@ -114,13 +116,14 @@ Provea derivacion de isMemberPF
 -}
 
 -- isMemberPF ::
--- isMemberPF :: Eq a => a -> [a] -> Bool
--- isMemberPF x xs = hasSomeHit (\y -> x == y) xs
--- isMemberPF x xs = (\y -> x == y) `hasSomeHit`  xs
--- isMemberPF x xs = (== x) `hasSomeHit`  xs
--- isMemberPF x xs = (`hasSomeHit` xs) (==x)
--- isMemberPF x xs = ((`hasSomeHit` xs) ==) x
-
+isMemberPF :: Eq a => a -> [a] -> Bool
+-- isMemberPF x xs = hasSomeHit (== x) xs
+-- isMemberPF x = hasSomeHit (== x)
+isMemberPF x = hasSomeHit . (==) $ x   
+-- isMemberPF x = (hasSomeHit . (==)) $ x
+-- isMemberPF x = (hasSomeHit . (==)) x
+-- isMemberPF = (hasSomeHit . (==)) 
+-- isMemberPF = hasSomeHit . (==)
 
 
 {-------------------------------------------}
@@ -141,25 +144,44 @@ testPay = describe "testPay function:" $ do
         property $ \n -> (n::Int) >= 8 ==> (3 * (fst (pay n))) + (5 * (snd (pay n))) == n
     
 testNumberOfHits :: Spec
-testNumberOfHits = describe "testNumberOfHits function:" $ do
-    it "" $
+testNumberOfHits = describe "testNumbehrOfHits function:" $ do
+    it "++ por sobre +" $
       property $ \xs ys -> numberOfHits even ((xs::[Int]) ++ (ys::[Int])) == numberOfHits even xs + numberOfHits even ys
-    it "" $
+    it "largo de xs" $
       property $ \xs -> numberOfHits (\_ -> True) (xs::[Int]) == length xs
-    it "" $
+    it "null" $
       property $ \xs -> numberOfHits (\_ -> False) (xs::[Int])  == 0
 
-{-
+
+trueP :: a -> Bool
+trueP _ = True
+
+intersection :: Eq a => [a] -> [a] -> [a]
+intersection [] _ = []
+intersection (x:xs) ys | isMember x ys = [x] ++ intersection xs ys
+                       | otherwise = intersection xs ys
+
+union :: Eq a => [a] -> [a] -> [a]
+union [] ys = ys
+union (x:xs) ys | isMember x ys = union xs ys
+                | otherwise = [x] ++ union xs ys
+
+unique :: Eq a => [a] -> [a]
+unique [] = []
+unique (x:xs) | isMember x xs = xs
+              | otherwise = [x] ++ unique xs
+
 testExtraNumberOfHits :: Spec
-testExtraNumberOfHits = describe "testNumberOfHits function:" $ do
-    it "" $
-      property $ 
--}
+testExtraNumberOfHits = describe "testExtraNumberOfHits function:" $ do
+    it "Inclusion-Exclusion" $
+      property $ \xs ys -> numberOfHits trueP (union (unique (xs::[Int])) (unique (ys::[Int]))) == 
+        (numberOfHits trueP (unique xs)) + (numberOfHits trueP (unique ys)) - numberOfHits trueP (intersection (unique xs) (unique ys))
+
 
 main:: IO()
 main = hspec $ do
   testEffPow
   testPay
   testNumberOfHits
-  -- testExtraNumberOfHits
+  testExtraNumberOfHits
 
