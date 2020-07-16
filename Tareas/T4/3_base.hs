7ddaf90b44da59d82838f3f4b98ef5391b449344
Amitsuu <joaquinpereza96@gmail.com>
2020-07-16 17:13:48 -0400

    P1 T4 Listo :o

--- Tareas/T4/3_base.hs
+++ Tareas/T4/3_base.hs
@@ -17,7 +17,8 @@ targetMatrix = [[14,16,15,13],[6,8,7,5],[2,4,3,1],[10,12,11,9]]
 
 -- parte (a)
 swapRow :: Int -> Int -> Matrix -> Matrix
-swapRow x y m = (swap x y) <$> m
+swapRow x y m = swap x y m
+
 
 swapColumn :: Int -> Int -> Matrix -> Matrix
 swapColumn x y m = m >>= \xs -> [swap x y xs] 
@@ -32,24 +33,21 @@ swap' first second lst = beginning ++ [y] ++ middle ++ [x] ++ end
     (beginning, (x : r)) = splitAt first lst
     (middle, (y : end)) = splitAt (second - first - 1) r
 
-
 -- parte (b)
 swapM :: Size -> Matrix -> [Matrix]
 swapM n m = do
   i <- [0..(n-1)]
   j <- [0..(n-1)]
   guard ((i/=j) && (i<j))
-  return (swapColumn i j m) -- Alerta!
+  (return (swapRow i j m)) ++ (return (swapColumn i j m))
 
 -- parte (c)
 swapMUntil :: Size -> (Matrix -> Bool) -> (Int, [Matrix]) -> (Int, [Matrix])
 swapMUntil n p (i, ms) | (filter p ms) /= [] = (i, (filter p ms))
                        | otherwise = swapMUntil n p (i+1, (ms >>= \xs -> swapM n xs))
+
 answer :: Int
 answer = fst (swapMUntil 4 (== targetMatrix) (0,[sourceMatrix]))
-
-
-
 
 
 {-------------------------------------------}
